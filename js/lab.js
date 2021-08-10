d868d716143700a409075518e25402f74fbc1b64
Julian Valencia (Bits Americas) <88675968+BitsJulianValencia@users.noreply.github.com>
2021-08-10 16:24:08 -0500

    Por favor, revisar e integrar estos cambios

--- js/lab.js
+++ js/lab.js
@@ -2,4 +2,5 @@ $(document).ready(function () {
     console.log('modificación introducida en custom_branch');
     console.log('Cambio hecho en master');
     console.log('Cambio hecho en custom_branch');
+    console.log('Modificación para integrar');
 });
